276c6a592084ffd1322109cc225a11b01d071a4e
K6NO <tamas.kenessey@gmail.com>
2017-10-18 16:26:22 +0200

    GET reads all folders async

--- routes/router_index.js
+++ routes/router_index.js
@@ -14,19 +14,45 @@ router.use('/', require('./router_login.js'));
 router.use('/', require('./router_download.js'));
 
 router.get('/', (req, res, next) => {
+    // async get list of album folders
     fs.readdirAsync(albumsFolder)
         .then((albums)=> {
             let albumsList = [];
+            let promiseStack = [];
+
+            // iterate list list of album folders
             for (let key in albums){
+
+                // exclude any files, other shit
                 if(albums[key].indexOf('.') === -1) {
+
+                    // create Album instances, push it to a list
                     let albumName = albums[key];
                     let albumDate = moment().format('YYYY');
                     let album = new Album({albumName: albumName, date: albumDate});
-                    album.albumCover = path.join('/img/albums/' + albumName + '/thumb/' + fs.readdirSync(path.join(albumsFolder, albumName, 'thumb'))[0]);
                     albumsList.push(album);
-                    }
+
+                    // async read the thumb subfolder in each album folder, push the promises to a list
+                    promiseStack.push(fs.readdirAsync(path.join(albumsFolder, albumName, 'thumb')));
                 }
-            return albumsList;
+            }
+            // resolve all concurrent promises at once --> will be several list of thumb files
+            return Promise.all(promiseStack).then((albumThumbsList)=> {
+
+                // iterate the resolved promises (list of thumb files)
+                for (let key in albumThumbsList) {
+
+                    // choose a random image from among the thumbs
+                    let randomiser = Math.floor(Math.random() * albumThumbsList[key].length);
+
+                    // add the albumCover on matching Album objects --> key in here needs to correspond to key in the above iteration
+                    // fortunately promiseStack resolves in the same order as the promises were pushed in the list above
+                    albumsList[key].albumCover = path.join('/img/albums/' + albumsList[key].albumName + '/thumb/' + albumThumbsList[key][randomiser]);
+
+
+                }
+                return albumsList;
+            });
         })
         .then((albumsList)=> {
             res.render('index', {
