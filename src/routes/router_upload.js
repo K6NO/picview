c820a21694a599c98f92cb1bd58b2012508fbb13
K6NO <tamas.kenessey@gmail.com>
2017-10-23 12:50:46 +0200

    heroku upload folder error bughunt5

--- src/routes/router_upload.js
+++ src/routes/router_upload.js
@@ -57,9 +57,9 @@ var upload = multer({
 });
 
 
-router.post('/upload', upload.array('image', 100), (req, res, next) => {
+router.post('/upload', (req,res,next)=> {    console.log('any logs????');
+}, upload.array('image', 100), (req, res, next) => {
 
-    console.log('any logs????');
     // MAGIC HAPPENS HERE - check upload folder, count exsiting albums, resize pictures, zip, delete files from upload folder
     fileOperations.resizeZipDelete(req.body.albumName);
     res.redirect('/');
